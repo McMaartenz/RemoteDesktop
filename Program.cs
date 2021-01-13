c1d803e18b0f63ee9f4ce38ee81767b83a100a0e
Maarten van Keulen <mcmaartenz@outlook.com>
2021-01-13 11:34:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteDesktop
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			Console.WriteLine("Hello, world!");
+			StartWindow.Start();
+		}
+	}
+}
---
+++ StartWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteDesktop
+{
+	public partial class StartWindow : Form
+	{
+		[STAThread]
+		public static void Start()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new StartWindow());
+		}
+
+		public StartWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
