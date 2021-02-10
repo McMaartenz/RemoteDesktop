000fb43e0a0c84d6d998cd680a9b6e41331c5277
Jeroen Mulders <18994@greijdanus.nl>
2021-02-10 10:32:34 +0100

    .

---
+++ Utility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace RemoteDesktop
+{
+    private void Capture(object)
+    {
+        
+        Bitmap Capture = new Bitmap(1280, 720, PixelFormat.Format32bppArgb);
+     
+    }
+}
